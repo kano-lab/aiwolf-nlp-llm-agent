1d4430f34ba43e180e6f71d4c5f3194d1650299a
navleorange <navleorange.0718@gmail.com>
2024-12-23 05:45:08 +0900

    add: プロンプト用のファイルを追加

---
+++ src/res/prompt.py
@@ -0,0 +1,20 @@
+from aiwolf_nlp_common.role import Role
+from aiwolf_nlp_common.protocol.list.talk_list import TalkList
+
+
+class Prompt:
+
+    @classmethod
+    def get_common_prompt(cls, agent_name:str, role:Role) -> str:
+        return f"""
+        あなたは人狼ゲームのプレイヤーの一員として、会話を行なってください。
+        あなたのゲーム中での名前は{agent_name}です。
+        あなたの役職は{role.ja}です。
+        """
+    
+    @classmethod
+    def get_talk_prompt(cls, talk_history:TalkList) -> str:
+        return f"""
+        以下は今までの会話履歴です。会話に次ぐ発言をしてください。
+        {"\n".join(talk_history)}
+        """
