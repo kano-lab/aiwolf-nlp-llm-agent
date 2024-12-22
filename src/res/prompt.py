cff41c8623b3f8617735a73bd8f06ea9ea3f0e97
navleorange <navleorange.0718@gmail.com>
2024-12-23 06:41:32 +0900

    add: format_text

--- src/res/prompt.py
+++ src/res/prompt.py
@@ -1,21 +1,34 @@
-from aiwolf_nlp_common.role import Role
+from __future__ import annotations
+
+import re
+from typing import Callable
+
 from aiwolf_nlp_common.protocol.list.talk_list import TalkList
+from aiwolf_nlp_common.role import Role
 
 
 class Prompt:
+    def format_text(func: Callable):
+        def _wrapper(*args, **keywords):
+            # execute function
+            result: str = func(*args, **keywords)
+
+            return re.sub(r"^\s+(.+)", r"\1", result, flags=re.MULTILINE)
+
+        return _wrapper
 
     @classmethod
-    def get_common_prompt(cls, agent_name:str, role:Role) -> str:
-        return f"""
-        あなたは人狼ゲームのプレイヤーの一員として、会話を行なってください。
+    @format_text
+    def get_common_prompt(cls, agent_name: str, role: Role) -> str:
+        return f"""あなたは人狼ゲームのプレイヤーの一員として、会話を行なってください。
         あなたのゲーム中での名前は{agent_name}です。
         あなたの役職は{role.ja}です。
         """
-    
+
     @classmethod
-    def get_talk_prompt(cls, talk_history:TalkList) -> str:
+    @format_text
+    def get_talk_prompt(cls, talk_history: TalkList) -> str:
         talk_history_text = "\n".join([f"{talk.agent}:{talk.text}" for talk in talk_history])
-        return f"""
-        以下は今までの会話履歴です。会話に次ぐ発言をしてください。
+        return f"""以下は今までの会話履歴です。会話に次ぐ発言をしてください。
         {talk_history_text}
         """
