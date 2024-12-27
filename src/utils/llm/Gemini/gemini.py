3a90b861cdf6340951be4e71932a9585228a4c2f
navleorange <navleorange.0718@gmail.com>
2024-12-27 20:31:47 +0900

    add: Gemini

--- src/utils/llm/Gemini/gemini.py
+++ src/utils/llm/Gemini/gemini.py
@@ -1,39 +1,75 @@
 from __future__ import annotations
 
+import configparser
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import google.generativeai as genai
-from google.generativeai.types import GenerationConfig
-
 from dotenv import load_dotenv
+from google.generativeai.types import GenerateContentResponse, GenerationConfig
 
 if TYPE_CHECKING:
-    import configparser
+    from google.generativeai.types import content_types
 
 
 class Gemini:
-    __config_key = "Gemini"
+    __config_key = "params"
 
-    def __init__(self, config: configparser.ConfigParser):
-        self.model: str = config.get(self.__config_key, "model")
+    def __init__(
+        self,
+        config: configparser.ConfigParser,
+        system_instruction: content_types.ContentType,
+    ) -> None:
+        self.load_api_key(config=config)
+        gemini_config = self.read_config(config=config)
+
+        self.model: str = gemini_config.get(self.__config_key, "model")
 
         self.optional_args: GenerationConfig = GenerationConfig(
-            candidate_count=config.getint(self.__config_key, "candidate_count", fallback=None),
-            max_output_tokens=config.getint(self.__config_key, "max_output_tokens", fallback=None),
-            temperature=config.getfloat(self.__config_key, "temperature", fallback=None),
-            top_p=config.getfloat(self.__config_key, "top_p", fallback=None),
-            top_k=config.getint(self.__config_key, "top_k", fallback=None),
-            seed=config.getint(self.__config_key, "seed", fallback=None),
-            presence_penalty=config.getfloat(self.__config_key, "presence_penalty", fallback=None),
-            frequency_penalty=config.getfloat(
-                self.__config_key, "frequency_penalty", fallback=None
+            candidate_count=gemini_config.getint(
+                self.__config_key,
+                "candidate_count",
+                fallback=None,
+            ),
+            max_output_tokens=gemini_config.getint(
+                self.__config_key,
+                "max_output_tokens",
+                fallback=None,
+            ),
+            temperature=gemini_config.getfloat(self.__config_key, "temperature", fallback=None),
+            top_p=gemini_config.getfloat(self.__config_key, "top_p", fallback=None),
+            top_k=gemini_config.getint(self.__config_key, "top_k", fallback=None),
+            seed=gemini_config.getint(self.__config_key, "seed", fallback=None),
+            presence_penalty=gemini_config.getfloat(
+                self.__config_key,
+                "presence_penalty",
+                fallback=None,
+            ),
+            frequency_penalty=gemini_config.getfloat(
+                self.__config_key,
+                "frequency_penalty",
+                fallback=None,
             ),
         )
 
         self.client = genai.GenerativeModel(
-            model_name=self.model, generation_config=self.optional_args
+            model_name=self.model,
+            generation_config=self.optional_args,
+            system_instruction=system_instruction,
         )
+        self.chat = self.client.start_chat(history=[])
+
+    @classmethod
+    def read_config(cls, config: configparser.ConfigParser) -> configparser.ConfigParser:
+        gemini_config_path: str = config.get("path", "gemini_config")
+
+        if not Path(gemini_config_path).is_file():
+            raise FileNotFoundError(gemini_config_path, "Geminiの設定ファイルが見つかりません")
+
+        gemini_config = configparser.ConfigParser()
+        gemini_config.read(gemini_config_path)
+
+        return gemini_config
 
     @classmethod
     def load_api_key(cls, config: configparser.ConfigParser) -> None:
@@ -43,3 +79,6 @@ class Gemini:
             raise FileNotFoundError(api_key_path, "APIの設定ファイルが見つかりません")
 
         load_dotenv(api_key_path)
+
+    def create_message(self) -> GenerateContentResponse:
+        return self.chat.send_message()
