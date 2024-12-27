d092258c882293bed33f35c1d91cdb354b4d8810
navleorange <navleorange.0718@gmail.com>
2024-12-27 22:00:45 +0900

    add: initでsystem_instructionを追加

--- src/utils/llm/ChatGPT/aiwolf_nlp_gpt.py
+++ src/utils/llm/ChatGPT/aiwolf_nlp_gpt.py
@@ -11,12 +11,19 @@ if TYPE_CHECKING:
 
 
 class AIWolfNLPChatGPT(ChatGPT):
-    def __init__(self, config: configparser.ConfigParser) -> None:
+    def __init__(self, config: configparser.ConfigParser, system_instruction:str) -> None:
         super().__init__(config=config)
+
+        self.add_system_message(content=system_instruction)
 
     def set_action_time_out(self, action_timeout: int) -> None:
         self.client.timeout = action_timeout
 
-    def create_comment(self) -> str:
+    def create_comment(self, content: str) -> str:
+        self.add_user_message(content=content)
+
         response: ChatCompletion = super().create_comment()
+
+        self.add_assistant_message(content=response.choices[0].message.content)
+
         return response.choices[0].message.content
